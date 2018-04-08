3229e8c047bd0d66574417bc85c948015043fe79
Unknown <eveiga@msn.com>
2018-04-08 16:16:15 -0300

    tudo funcionando, dados brutos
    pendendo merge com código do altimetro

--- gy85.ino
+++ gy85.ino
@@ -10,6 +10,11 @@
 #define ACCYHI 0x35
 #define ACCZLO 0x36
 #define ACCZHI 0x37
+
+#define GYROS 0x68
+#define GYR_R 0x1B
+
+
 #include <Wire.h>
 
 /*
@@ -21,18 +26,30 @@
 int MAGx,GYRx,ACCx;
 int MAGy,GYRy,ACCy;
 int MAGz,GYRz,ACCz;
+int temp;
+float tempC;
 
 
 
 void setup(){
-  Serial.begin(9600);
-  Wire.begin();
+	Serial.begin(9600);
+	Wire.begin();
+    
+	Wire.beginTransmission(MAGNT); //start talking
+  	
+	Wire.write(0x02); // Set the Register
+	Wire.write(0x00); // Tell the HMC5883 to Continuously Measure
   
-  
-  Wire.beginTransmission(MAGNT); //start talking
-  Wire.write(0x02); // Set the Register
-  Wire.write(0x00); // Tell the HMC5883 to Continuously Measure
-  Wire.endTransmission();
+	int j = Wire.endTransmission();
+		switch (j){
+		case 0: Serial.println("MAGNT SETUP OK"); break;
+		case 1: Serial.println("MAGNT SETUP TRANSMIT BUFF OVF"); break;
+		case 2: Serial.println("MAGNT SETUP ADDRESS NACK"); break;
+		case 3: Serial.println("MAGNT SETUP DATA NACK"); break;
+		case 4: Serial.println("MAGNT SETUP BELGIUM"); break;
+	}
+
+// ====================================================================
 
 	Wire.beginTransmission(ACCEL);
 
@@ -48,8 +65,30 @@ void setup(){
 		Wire.write(0x31); // resolução completa, escala +/- 2g
 		Wire.write(0x08);  
 
-	Wire.endTransmission();
-	delay(500);
+		int k = Wire.endTransmission();
+		switch (k){
+		case 0: Serial.println("ACCEL SETUP OK"); break;
+		case 1: Serial.println("ACCEL SETUP TRANSMIT BUFF OVF"); break;
+		case 2: Serial.println("ACCEL SETUP ADDRESS NACK"); break;
+		case 3: Serial.println("ACCEL SETUP DATA NACK"); break;
+		case 4: Serial.println("ACCEL SETUP BELGIUM"); break;
+	}
+
+// ===================================================================
+
+
+	Wire.beginTransmission(GYROS);
+		Wire.write(0x16);
+		Wire.write(0x1C);
+	int i = Wire.endTransmission();
+	switch (i){
+		case 0: Serial.println("GYROS SETUP OK"); break;
+		case 1: Serial.println("GYROS SETUP TRANSMIT BUFF OVF"); break;
+		case 2: Serial.println("GYROS SETUP ADDRESS NACK"); break;
+		case 3: Serial.println("GYROS SETUP DATA NACK"); break;
+		case 4: Serial.println("GYROS SETUP BELGIUM"); break;
+	}
+
 }
 
 
@@ -72,7 +111,31 @@ void loop(){
 		MAGy = Wire.read()<<8; //MSB y
 		MAGy |= Wire.read(); //LSB y
  	}
+//--------------------------------------------------
 
+// ------------------ LER GYROS -----------------------------
+
+	Wire.beginTransmission(GYROS);
+	Wire.write(GYR_R); //start with register 3.
+	Wire.endTransmission();
+	
+	Wire.requestFrom(GYROS, 8);
+
+	if(Wire.available() == 8){
+
+		temp = Wire.read()<<8; //MSB  x 
+		temp |= Wire.read(); //LSB  x
+		GYRx = Wire.read()<<8; //MSB  z
+		GYRx |= Wire.read(); //LSB z
+		GYRy = Wire.read()<<8; //MSB y
+		GYRy |= Wire.read(); //LSB y
+		GYRz = Wire.read()<<8; //MSB y
+		GYRz |= Wire.read(); //LSB y
+
+ 	}
+
+	tempC = temp/280 + 82.142857;
+//--------------------------------------------------
 
 // ------------------ LER ACELEROMETRO -----------------------------
 
@@ -121,21 +184,27 @@ unsigned long int tf = micros();
 
 
 // --------------------------------- Cuspir resultados
-
-
-	Serial.print(MAGx);
-	Serial.print(",");
-	Serial.print(MAGy);
-	Serial.print(",");
-	Serial.print(MAGz);
-	Serial.print(",");
 	Serial.print(ACCx);
 	Serial.print(",");
 	Serial.print(ACCy);
 	Serial.print(",");
 	Serial.print(ACCz);
 	Serial.print(",");
+	Serial.print(GYRx);
+	Serial.print(",");
+	Serial.print(GYRy);
+	Serial.print(",");
+	Serial.print(GYRz);
+	Serial.print(",");
+	Serial.print(MAGx);
+	Serial.print(",");
+	Serial.print(MAGy);
+	Serial.print(",");
+	Serial.print(MAGz);
+	Serial.print(",");
 	Serial.print(dt);
+	Serial.print(",");
+	Serial.print(tempC);
 	Serial.println(" ");
 
 }
